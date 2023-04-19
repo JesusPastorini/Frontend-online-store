3b4ce2b3e2cbbde2ee840f501ca2abf677015815
Nathaniel <naniidalpozzo@gmail.com>
2023-04-19 15:52:12 -0300

    REQ1 completo

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,15 @@
 export async function getCategories() {
   // Implemente aqui
+  const URL = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const categorias = await URL.json();
+  return categorias;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  const URL = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const categorias = await URL.json();
+  return categorias;
 }
 
 export async function getProductById() {
