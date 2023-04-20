fe132f253f5304b93c35a3053c42d3bee8bf5329
JesusPastorini <juniorpastorini_@hotmail.com>
2023-04-20 03:33:13 -0300

    requisito 05 ok

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,11 +4,7 @@ import Categories from './Categories';
 import { getProductsFromCategoryAndQuery } from '../services/api';
 
 class Home extends Component {
-  state = { produtos: [], pesquisa: '', };
-
-  componentDidMount() {
-    this.getProduct();
-  }
+  state = { produtos: [], pesquisa: '', visuaProd: false };
 
   onChange = ({ target }) => {
     const { value, name } = target;
@@ -20,41 +16,55 @@ class Home extends Component {
   getProduct = async () => {
     const { pesquisa } = this.state;
     const produtos = await getProductsFromCategoryAndQuery(' ', pesquisa);
-    this.setState({ produtos });
-    
+    const { results } = produtos;
+    this.setState({ produtos: results, pesquisa, visuaProd: true });
+    console.log(results);
   };
 
   render() {
-    const { produtos } = this.state;
+    const { produtos, visuaProd } = this.state;
     return (
 
       <div>
+        <input
+          placeholder="pesquisar"
+          type="text"
+          name="pesquisa"
+          data-testid="query-input"
+          onChange={ this.onChange }
+        />
+        <button
+          type="button"
+          data-testid="query-button"
+          onClick={ this.getProduct }
+        >
+          Pesquisar
+        </button>
+        {
+          !visuaProd ? (
+            <p data-testid="home-initial-message">
+              Digite algum termo de pesquisa ou escolha uma categoria.
+            </p>
+          ) : (
+            <div>
+              {produtos.length > 0 ? (
+                produtos.map((prod) => (
+                  <div data-testid="product" key={ prod.id }>
+                    <h2>{ prod.title }</h2>
+                    <img src={ prod.thumbnail } alt={ prod.title } />
+                    <p>{ `R$ ${prod.price}` }</p>
+                  </div>
+                ))) : (<span>Nenhum produto foi encontrado</span>)}
+            </div>)
+        }
         <div>
-          <input
-            placeholder="pesquisar"
-            type="text"
-            name="pesquisa"
-            data-testid="query-input"
-            onChange={ this.onChange }
-          />
-          <button
-            type="button"
-            data-testid="query-button"
-            onClick={ this.getProduct }
-          >
-            pesquisar
-          </button>
-
+          <div>
+            <Link to="/ShoppingCart" data-testid="shopping-cart-button">
+              search
+            </Link>
+          </div>
+          <Categories />
         </div>
-        <p data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </p>
-        <div>
-          <Link to="/ShoppingCart" data-testid="shopping-cart-button">
-            search
-          </Link>
-        </div>
-        <Categories />
       </div>
     );
   }
