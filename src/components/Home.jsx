9e87a89b61d173512e1511c90e2b65f8420c33d9
Guilherme Muniz <guilherme.muniz91@gmail.com>
2023-04-21 15:15:20 -0300

    correção no requisito 3 que quebrou após o commit do req6

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
 
 class Home extends Component {
@@ -57,6 +58,9 @@ class Home extends Component {
     ));
     return (
       <div className="homepage-div">
+        <Link to="/ShoppingCart" data-testid="shopping-cart-button">
+          Carrinho de compras
+        </Link>
         <div>
           <button
             type="button"
